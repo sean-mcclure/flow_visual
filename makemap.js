9a79de354d3da04a42e9fd6ecab30e837dc17f6f
Sean McClure <seanaaron100@gmail.com>
2021-04-02 22:13:15 +0000

    changing approach

---
+++ makemap.js
@@ -0,0 +1,12 @@
+function makemap() {
+    <ul>
+  <li><a>Main Topic</a>
+    <ul>
+      <li><a>Sub 2</a></li>
+      <li><a>Sub 3</a></li>
+      <li><a>Sub 4</a></li>
+      <li><a>Sub 5</a></li>
+    </ul>
+  </li>
+</ul>
+}
