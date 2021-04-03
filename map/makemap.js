6f123801541454a2408364365d0c8e5f863c9d61
Sean McClure <seanaaron100@gmail.com>
2021-04-03 00:54:01 +0000

    recursive layers3

---
+++ map/makemap.js
@@ -0,0 +1,32 @@
+var data = [{
+    layer: 1,
+    name: "hello",
+    id: "d9s8f7a"
+}, {
+    layer: 2,
+    name: "second layer1",
+    id: "35jnk354",
+    connected_to: "d9s8f7a"
+}, {
+    layer: 2,
+    name: "second layer2",
+    id: "fs0d9f9f",
+    connected_to: "d9s8f7a"
+}, {
+    layer: 2,
+    name: "second layer3",
+    id: "n78gn686n",
+    connected_to: "d9s8f7a"
+}, {
+    layer: 2,
+    name: "second layer4",
+    id: "g8f9gfg08f",
+    connected_to: "d9s8f7a"
+},
+{
+    layer: 3,
+    name: "third layer1",
+    id: "ds08d90fds",
+    connected_to: "g8f9gfg08f"
+}]
+
